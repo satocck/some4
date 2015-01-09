1c4364f69f1fe6f0fe38888df55c740d15da1710
satocck <juliancabuya@CGTMAED-iMac-5.local>
2015-01-09 12:49:58 -0700

    Initial Commit

---
+++ some4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  some4
+//
+//  Created by julian cabuya on 1/9/15.
+//  Copyright (c) 2015 julian cabuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
